30ffb226adee2d8c17761d3cfd377f46b046a49c
Sammy Breen <s.breen@tum.de>
2020-09-20 13:01:33 +0200

    first implementation of route class, used do read coordinates for transport

---
+++ modules/route/route.py
@@ -0,0 +1,55 @@
+import os
+from datetime import date, datetime 
+from math import sqrt, floor
+import geopy.distance
+
+import numpy as np
+import pandas as pd
+
+class Route:
+    """Class to represent a route. Saves coordinates and start and end time."""
+    def __init__(self, start: datetime, end: datetime, filename: str):
+        self.start = start
+        self.end = end
+        self.traveltime = end - start
+        self.read(filename)
+        self.distance = self.calc_distance()
+        self.coordinates = self.coordinates_hourly()
+
+    def calc_distance(self):
+        """Calculate the total distance of the route"""
+        distance = 0.0
+        for i in range(len(self.coordinates_full) - 1):
+            coords_1 = self.coordinates_full[i]
+            coords_2 = self.coordinates_full[i+1]
+            distance = distance + geopy.distance.distance(coords_1, coords_2).km
+
+        return distance
+
+    def read(self, filename: str):
+        """Read coordinates from a .csv file"""
+        df = pd.read_csv(filename, usecols=[0,1], names=['Lon', 'Lat'])
+        self.coordinates_full =  df[['Lat', 'Lon']].values
+
+    def coordinates_hourly(self):
+        """Calculate the coordinates of the route for every hour"""
+        # Travel speed in km/h
+        speed = 80
+
+        # Initialize variables
+        coordinates = np.zeros([floor(self.distance/speed), 2])
+        counter = 0
+        distance = 0.0
+
+        for i in range(len(self.coordinates_full) - 1):
+            coords_1 = self.coordinates_full[i]
+            coords_2 = self.coordinates_full[i+1]
+            distance = distance + geopy.distance.distance(coords_1, coords_2).km
+
+            # Save coordinates after one hour of traveltime
+            if distance > speed * 1:
+                coordinates[counter, :] = coords_2
+                counter = counter + 1
+                distance = 0
+
+        return coordinates
